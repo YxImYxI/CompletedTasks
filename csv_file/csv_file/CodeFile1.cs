3329f8d9c6fa7c06d93a67c00ade57e88b820aad
YxImYxI <mr.sver@bk.ru>
2024-10-18 15:55:47 +0700

    Добавленно выравнивание по длине

---
+++ csv_file/csv_file/CodeFile1.cs
@@ -0,0 +1,43 @@
+﻿using System;//основные классы
+using System.IO;//для работы с файлами
+
+class CodeFile1
+{
+    static void Main(string[] args)
+    {
+        //Ссылка на файл
+        string URL = "C:\\work\\CompletedTasks\\csv_file\\csv_file\\example.csv";
+        //проверка на наличие файла
+        if (File.Exists(URL))
+        {
+            // Чтение файла
+            string[] lines = File.ReadAllLines(URL);
+            int maxLength = 0;
+
+            // Находим максимальную длину строк
+            foreach (var line in lines)
+            {
+                var columns = line.Split(',');
+                foreach (var column in columns)
+                {
+                    maxLength = Math.Max(maxLength, column.Trim().Length);
+                }
+            }
+
+            // Вывод с одинаковым размером
+            foreach (var line in lines)
+            {
+                var columns = line.Split(',');
+                foreach (var column in columns)
+                {
+                    Console.Write($"|{column.Trim().PadRight(maxLength)} ");
+                }
+                Console.WriteLine("|");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файл не найден");
+        }
+    }
+}
