337062fc8df5b5c81b6c6549c15126c2cade22ba
YxImYxI <mr.sver@bk.ru>
2024-10-18 15:52:07 +0700

    консольное приложение, которое читает содержимое csv-файла с диска и выдает на экран в виде таблицы

---
+++ csv_file/csv_file/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string URL = "C:\\work\\CompletedTasks\\csv_file\\csv_file\\example.csv";
+
+        if (File.Exists(URL))
+        {
+            // Чтение всех линий файла
+            string[] lines = File.ReadAllLines(URL);
+            foreach (var line in lines)
+            {
+                var columns = line.Split(','); // разделяем строку по запятой
+                foreach (var column in columns)
+                {
+                    Console.Write($"|{column.Trim()} "); // удаляем лишние пробелы
+                }
+                Console.WriteLine("|");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файл не найден");
+        }
+    }
+}
